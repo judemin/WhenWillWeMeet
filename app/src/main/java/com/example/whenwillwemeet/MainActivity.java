51719532b057b2dd7d0eaef891921cc793da6f2d
민상연 <judemin2087@naver.com>
2020-02-27 14:42:34 +0900

    Google Login

--- app/src/main/java/com/example/whenwillwemeet/MainActivity.java
+++ app/src/main/java/com/example/whenwillwemeet/MainActivity.java
@@ -1,27 +1,36 @@
 package com.example.whenwillwemeet;
 
+import android.content.Intent;
+import android.net.wifi.hotspot2.pps.Credential;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.database.Cursor;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.*;
+import com.squareup.timessquare.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference("message");
     private View mainLayout;
+    public static CalendarPickerView calendarPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +42,31 @@ public class MainActivity extends AppCompatActivity {
         mainLayout = findViewById(R.id.mainlayout);
         FloatingActionButton fab = findViewById(R.id.fab);
 
+        // 사용자 이름
+        /*
+        Cursor c = getApplication().getContentResolver().query(ContactsContract.Profile.CONTENT_URI, null, null, null, null);
+        c.moveToFirst();
+        databaseReference.setValue((c.getString(c.getColumnIndex("display_name"))));
+        c.close();
+        */
+        //
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 googleLogin();
             }
         });
+
+        // Calendar
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 1);
+
+        calendarPicker = (CalendarPickerView) findViewById(R.id.calendar_view);
+        Date today = new Date();
+        calendarPicker.init(today, nextYear.getTime())
+                .withSelectedDate(today);
+        //
     }
 
     @Override
@@ -66,6 +94,11 @@ public class MainActivity extends AppCompatActivity {
     public void googleLogin(){
         Snackbar.make(mainLayout, "googleLogin", Snackbar.LENGTH_LONG).show();
 
-        databaseReference.setValue("Hello World!");
+
+
+        Intent loginIntent = new Intent(getApplicationContext(), GoogleLogin.class);
+        startActivity(loginIntent);
     }
+
+
 }
