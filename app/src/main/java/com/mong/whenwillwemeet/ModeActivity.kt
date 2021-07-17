77c31b54e7178a30df12e060f1e26cb4646455fc
민상연 <judemin2087@naver.com>
2021-07-17 16:01:14 +0900

    feat : Passwd Dialog
    - ModeActivity
    - dialog layout

--- app/src/main/java/com/mong/whenwillwemeet/ModeActivity.kt
+++ app/src/main/java/com/mong/whenwillwemeet/ModeActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ModeActivity : AppCompatActivity() {
 
@@ -34,10 +37,35 @@ class ModeActivity : AppCompatActivity() {
             intent.putExtra("user", nowUser)
             startActivity(intent)
         }
+
+        findBtn.setOnClickListener {
+            val inputCode : EditText = findViewById(R.id.mode_inputcode_et)
+            val roomID = inputCode.text
+
+            val database = Firebase.database
+            val nowRef = database.getReference("" + roomID)
+
+            nowRef.get().addOnSuccessListener {
+
+                // popup dialog 추가해야 함
+
+                val intent = Intent(this, SelectDayActivity::class.java)
+                intent.putExtra("user", nowUser)
+                intent.putExtra("roomID", roomID)
+                startActivity(intent)
+
+            }.addOnFailureListener {
+                makeToast("약속이 없는거 같은데..?ㅠㅜ")
+            }
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Toast.makeText(applicationContext,"안녕, 다음엔 꼭 한번 보자", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun makeToast(msg : String){
+        Toast.makeText(applicationContext,"" + msg, Toast.LENGTH_SHORT).show()
     }
 }
