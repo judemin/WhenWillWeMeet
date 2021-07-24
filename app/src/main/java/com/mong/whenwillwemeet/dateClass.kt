69e1a2ef9591a330f7e724465a6b6e530e9ab925
민상연 <judemin2087@naver.com>
2021-07-24 15:22:25 +0900

    update : dateClass
    - Static funtion for comparison

--- app/src/main/java/com/mong/whenwillwemeet/dateClass.kt
+++ app/src/main/java/com/mong/whenwillwemeet/dateClass.kt
@@ -19,4 +19,14 @@ class dateClass {
         hour = c.get(Calendar.HOUR)
         min = c.get(Calendar.MINUTE)
     }
+
+    fun isSameDateClass(src : dateClass, dest : dateClass) : Boolean{
+        if(src.year != dest.year)
+            return false
+        if(src.month != dest.month)
+            return false
+        if(src.day != dest.day)
+            return false
+        return true
+    }
 }
